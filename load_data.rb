abb93adc2ff621019f1ee0ac5c880b6fe45aa0d3
Tom Morris <tom@tommorris.org>
2011-10-06 11:44:39 +0100

    uploading tasks.xml, part of data as well as xquery

--- load_data.rb
+++ load_data.rb
@@ -1,4 +1,6 @@
 Dir.glob("db/linkedgov/*/**").each do |file|
   target_filename = file.gsub(/\.rdf$/, "")
+  puts target_filename
   `curl -H "Content-Type: application/xml" --upload-file #{file} http://localhost:8080/exist/rest/#{target_filename}`
 end
+`curl -H "Content-Type: application/xml" --upload-file db/linkedgov-meta/taskhopper/tasks.xml http://localhost:8080/exist/db/linkedgov-meta/taskhopper/tasks.xml`
